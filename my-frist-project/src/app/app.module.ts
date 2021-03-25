4a01a5c86e7edb1573b509ba8cbf1903b892326d
Kayleigh-Redler <49678122+Kayleigh-Redler@users.noreply.github.com>
2021-03-25 14:01:45 +0000

    Scrimba - Services & HTTP

--- my-frist-project/src/app/app.module.ts
+++ my-frist-project/src/app/app.module.ts
@@ -1,23 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CustomerModule } from './customers/customers.module';
-import { CustomersComponent } from './customers/customers.component';
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { CustomerModule } from './customers/customers.module';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CustomerModule,
-    SharedModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, CoreModule, CustomerModule, SharedModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
