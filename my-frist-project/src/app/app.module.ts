194b2de2c3c53a129c25067c70d1fa44bf7e2afc
Kayleigh-Redler <49678122+Kayleigh-Redler@users.noreply.github.com>
2021-03-25 14:42:19 +0000

    Scrimba - Routing

--- my-frist-project/src/app/app.module.ts
+++ my-frist-project/src/app/app.module.ts
@@ -6,8 +6,10 @@ import { SharedModule } from './shared/shared.module';
 import { CustomerModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  imports:      [ BrowserModule, CoreModule, CustomerModule, SharedModule ],
+  imports:      [ BrowserModule, CoreModule, CustomerModule, SharedModule, AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
