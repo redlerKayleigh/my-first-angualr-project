3400ccb3c4fa4da373560461bdaa0ece74281b22
Kayleigh-Redler <49678122+Kayleigh-Redler@users.noreply.github.com>
2021-03-25 14:44:48 +0000

    Scrimba - Router Outlet

--- my-frist-project/src/app/app.component.ts
+++ my-frist-project/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-customers></app-customers>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
